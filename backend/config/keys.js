b8ab9348ad26a288918e77ea67bf12138a607d9b
ZahiraGhazoul <zahiraghazoul919@gmail.com>
2023-06-02 15:42:58 +0100

    first commit

---
+++ backend/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //mongoURI : "mongodb+srv://zahiraghazoul919:za2000hira@cluster0.wn7xnda.mongodb.net/?retryWrites=true&w=majority"
+    //mongoURI : "mongodb://localhost:27017"
+    mongoURI : "mongodb+srv://zahiraghazoul919:za2000hira@cluster0.wn7xnda.mongodb.net/"
+}
